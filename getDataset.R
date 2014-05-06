b247a3f6fd234cc4b5c85579818cadf2a00d0ea8
Matias Lespiau <mlespiau@avature.net>
2014-05-06 09:45:49 -0300

    Filter the dataset by date

--- getDataset.R
+++ getDataset.R
@@ -8,6 +8,8 @@ getDataset <- function() {
     download.file(datasetUrl, destfile=destFile, method="curl")
   }
   unzip(destFile, exdir="./data/", overwrite=TRUE)
-  data <- read.csv("./data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+  rawData <- read.csv("./data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+  rawData$Date <- as.Date(rawData$Date, format="%d/%m/%Y")
+  twoDaysData <- subset(rawData, Date >= "2007-02-01" & Date <= "2007-02-02")
 }
 
