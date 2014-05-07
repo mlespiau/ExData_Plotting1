34fff7040e11f4ec4e9628cd15c7c03341aec2c5
Matias Lespiau <mlespiau@avature.net>
2014-05-06 21:44:14 -0300

    Add plot3.png and the script to create it

---
+++ plot3.R
@@ -0,0 +1,8 @@
+source("getDataset.R")
+data <- getDataset()
+png("plot3.png", height=480, width=480, bg="transparent")
+with(data, plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+with(data, lines(DateTime, Sub_metering_2, col="red"))
+with(data, lines(DateTime, Sub_metering_3, col="blue"))
+legend("topright", lty=c(1,1,1), col=c("black", "blue","red"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
